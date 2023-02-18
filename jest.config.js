a54987d21f643225e9905026565ab517616d5040
ligmanstark <57450873+ligmanstark@users.noreply.github.com>
2023-02-18 12:07:54 +0400

    test-factoring

--- jest.config.js
+++ jest.config.js
@@ -1,16 +1,20 @@
 module.exports = {
+	verbose: true,
 	roots: ['./lib/tests'],
 	transform: {
-		'^.+\\.js$': 'babel-jest',
+		'\\.js$': '<rootDir>/node_modules/babel-jest',
 		'.+\\.(css|styl|less|sass|scss)$': 'jest-transform-css',
+		"^.+\\.svg$": "<rootDir>/svgTransform.js",
+		"^.+\\.png$": "<rootDir>/pngTransform.js"
 	},
 	moduleNameMapper: {
 		moduleNameMapper: {
-			'\\.(css|less|sass|scss)$': '<rootDir>/__mocks__/styleMock.js',
-			'\\.(gif|ttf|eot|svg)$': '<rootDir>/__mocks__/fileMock.js',
+			'\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+				'<rootDir>/__mocks__/fileMock.js',
+			'\\.(css|less)$': '<rootDir>/__mocks__/styleMock.js',
 		},
 	},
-	moduleFileExtensions: ['ts', 'tsx', 'js', 'json'],
+	moduleFileExtensions: ['ts', 'tsx', 'js', 'json', 'jsx'],
 	globals: {
 		window: {},
 	},
